ede35137e4b6b96b0f830d4163ac02a6123f711a
Geno Roupsky <geno@roupsky.name>
2015-12-02 18:46:23 +0200

    first draft extracted from captive-portal

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### 0.0.1 (2 Dec 2015)
+
+* Project initiated
---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by groupsky on 02.12.15.
+ */
+
+var angular = require('angular');
+
+var app = angular.module('app', []);
+
+module.exports = app;
