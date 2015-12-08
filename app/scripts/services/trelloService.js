94c632f91d863d17879f12ef26eada4b4cfb3016
Geno Roupsky <geno@roupsky.name>
2015-12-08 16:30:38 +0200

    fixed CORS requests by using JSONP

--- app/scripts/services/trelloService.js
+++ app/scripts/services/trelloService.js
@@ -5,13 +5,23 @@
 require('../main').service('trelloService', /*@ngInject*/function ($http, config) {
     return {
         getCards: function () {
-            return $http.get('//api.trello.com/1/boards/' + config.boardId + '/cards/open')
+            return $http.jsonp('//api.trello.com/1/boards/' + config.boardId + '/cards/open', {
+                    params: {
+                        key: config.trelloAppKey,
+                        callback: 'JSON_CALLBACK'
+                    }
+                })
                 .then(function (response) {
                     return response.data;
                 });
         },
         getCardAttachment: function (cardId, attachmentId) {
-            return $http.get('//api.trello.com/1/cards/' + cardId + "/attachments/" + attachmentId)
+            return $http.jsonp('//api.trello.com/1/cards/' + cardId + "/attachments/" + attachmentId, {
+                    params: {
+                        key: config.trelloAppKey,
+                        callback: 'JSON_CALLBACK'
+                    }
+                })
                 .then(function (response) {
                     return response.data;
                 });
