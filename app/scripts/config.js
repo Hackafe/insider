8be3bea191735b1525c374d4832ebfc8f4a1f72d
Geno Roupsky <geno@roupsky.name>
2015-12-17 18:22:50 +0200

    fix labels

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -4,12 +4,22 @@
 
 require('./main').constant('config', {
     boardId: 'GHda069L',
+    skipLabels: [
+        // вътрешна среща на Хакафе
+        "565afa7dfb396fe706bec1d9",
+        // събитие на Programista;
+        "565af9ebfb396fe706bec184"
+    ],
     eventsLabelId: // "единична IT Лекция"
         '54648a0274d650d567a72353',
     coursesLabelId: // "Лекция от курс по IT"
         '54648a0274d650d567a72355',
-    gatheringsLabelId: // "Парти"
-        '565af8f0fb396fe706bec112',
+    gatheringsLabelId: [
+        // "Парти"
+        '566aca77fb396fe706d2928a',
+        // "Тематични сбирки"
+        '565af8f0fb396fe706bec112'
+    ],
     othersLabels: [
         // "Събитие на партньори"
         '565af54bfb396fe706bebf3d',
@@ -18,6 +28,7 @@ require('./main').constant('config', {
         // "Събитие на Икономическа библиотека"
         '54648a0274d650d567a72357',
         // "Външно събитие"
-        '54648a0274d650d567a72356'],
+        '54648a0274d650d567a72356'
+    ],
     trelloAppKey: '545c3e7e39e96a7544eae99bca688619'
 });
